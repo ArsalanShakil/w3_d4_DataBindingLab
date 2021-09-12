3517fdcdf4f196495f5ea0148971379ac945b0d7
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-09-12 15:07:34 +0300

    version 1.1

---
+++ app/src/main/java/com/example/w3_d4_databindinglab/CounterFragment.kt
@@ -0,0 +1,31 @@
+package com.example.w3_d4_databindinglab
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CounterFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CounterFragment()
+    }
+
+    private lateinit var viewModel: CounterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.counter_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CounterViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/w3_d4_databindinglab/CounterViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.w3_d4_databindinglab
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class CounterViewModel : ViewModel() {
+    private var counter: MutableLiveData<String> = MutableLiveData("0")
+    private var increment: MutableLiveData<Int> = MutableLiveData(0)
+
+    fun getCounter(): MutableLiveData<String>? {
+        return counter
+    }
+
+    fun setCounter(counter : String)
+    {
+        this.counter.value = counter
+    }
+
+    fun getIncrement(): MutableLiveData<Int>?
+    {
+        return increment
+    }
+
+    fun setIncrement(increment : Int)
+    {
+        this.increment?.postValue(increment)
+    }
+
+    fun incCounter() {
+        counter?.postValue((counter!!.value!!.toInt() + increment!!.value!!).toString()) // this can be called from another coroutine
+    }
+}
