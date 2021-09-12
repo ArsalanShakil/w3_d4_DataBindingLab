17c7a73743bad8ad2ab264631d35b2d0cc12e91a
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-09-12 15:12:09 +0300

    version 1.2

--- app/src/main/java/com/example/w3_d4_databindinglab/MainActivity.kt
+++ app/src/main/java/com/example/w3_d4_databindinglab/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.w3_d4_databindinglab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<CounterFragment>(R.id.fragmentContainerView)
+            }
+        }
     }
 }
